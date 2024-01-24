b61bc2704e74dd10dde0abd18e0a288eae3253fe
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-24 16:16:21 -0600

    [ADD] Dependency Injection

--- basics/TDD/dependencyInjection/main_test.go
+++ basics/TDD/dependencyInjection/main_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	t.Run("Adding testing", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		Greet(&buffer, "Chris")
+		got := buffer.String()
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("Got: %q Want: %q", got, want)
+		}
+	})
+}

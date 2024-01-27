cc9447c3a2116dac1d73dcf68b26754f5018485e
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-27 14:58:10 -0600

    [ADD] selected channels

--- basics/TDD/selected/main.go
+++ basics/TDD/selected/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
+
+var tenSecondTimeout = 10 * time.Second
 
 func Racer(a string, b string) (winner string) {
 	aTimeLapsed := measureResponseTime(a)
@@ -18,4 +21,28 @@ func measureResponseTime(url string) time.Duration {
 	start := time.Now()
 	http.Get(url)
 	return time.Since(start)
+}
+
+func Racer2(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+// ! Always make channels
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }

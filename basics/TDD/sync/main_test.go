a396fcfd6f3b7ba22dea90f89e65bbeea07b76d1
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-27 18:50:20 -0600

    [ADD] TDD Sync go rutines vs sync await

---
+++ basics/TDD/sync/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func TestCounter(t *testing.T) {
+
+	t.Run("Increment 3 times leave it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+	t.Run("Runs in a concurrent env", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}

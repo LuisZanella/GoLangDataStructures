d351449095f158a2cc2476dfc76c7905d7f76136
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-27 20:29:15 -0600

    [Fix] Fix MergeSort approach

--- easy/88_MergeSortedArray/main.go
+++ easy/88_MergeSortedArray/main.go
@@ -1,44 +1,26 @@
 package main
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-
-	if m == 0 {
-		for k := 0; k < n; k++ {
-			nums1[k] = nums2[k]
-		}
-		return
-	}
-	nums1 = moveToEnd(nums1, m)
-	i := n
-	j := 0
-	for k := 0; k < m+n; k++ {
-		if i < m+n && j < n {
-			if nums1[i] < nums2[j] {
-				nums1[k] = nums1[i]
-				i++
-			} else {
-				nums1[k] = nums2[j]
-				j++
-			}
-		} else if j < n {
-			nums1[k] = nums2[j]
-			j++
+	p1 := m - 1
+	p2 := n - 1
+	for i := m + n - 1; i >= 0; i-- {
+		if p2 < 0 {
+			break
 		}
 
+		if p1 < 0 {
+			nums1[i] = nums2[p2]
+			p2--
+			continue
+		}
+		l := nums1[p1]
+		r := nums2[p2]
+		if r >= l {
+			nums1[i] = r
+			p2--
+		} else {
+			nums1[i] = l
+			p1--
+		}
 	}
-	return
-
-}
-
-func moveToEnd(nums []int, m int) []int {
-	lenNums := len(nums)
-
-	k := lenNums
-
-	for i := m - 1; i >= 0; i-- {
-		nums[k-1] = nums[i]
-		k--
-	}
-
-	return nums
 }
--- easy/88_MergeSortedArray/main_test.go
+++ easy/88_MergeSortedArray/main_test.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assert(t *testing.T, got, want []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: %v Want: %v", got, want)
+	}
+}
+func TestMerge(t *testing.T) {
+	var a1, a2 []int
+	var m, n int
+	t.Run("nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3", func(t *testing.T) {
+		a1 = []int{1, 2, 3, 0, 0, 0}
+		a2 = []int{2, 5, 6}
+		m = 3
+		n = 3
+		merge(a1, m, a2, n)
+		want := []int{1, 2, 2, 3, 5, 6}
+		assert(t, a1, want)
+	})
+	t.Run("nums1 = [1], m = 1, nums2 = [], n = 0", func(t *testing.T) {
+		a1 = []int{1}
+		a2 = []int{}
+		m = 1
+		n = 0
+		merge(a1, m, a2, n)
+		want := []int{1}
+		assert(t, a1, want)
+	})
+	t.Run("nums1 = [0], m = 0, nums2 = [1], n = 1", func(t *testing.T) {
+		a1 = []int{0}
+		a2 = []int{1}
+		m = 0
+		n = 1
+		merge(a1, m, a2, n)
+		want := []int{1}
+		assert(t, a1, want)
+	})
+
+}

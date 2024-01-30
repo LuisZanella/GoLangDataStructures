9354ec3018d59cf9bd36054a0349093ec9ee9503
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-30 10:54:30 -0600

    [ADD] Check if Tree is symetric on iterative way

--- easy/101_LTC_SymetricTree/main.go
+++ easy/101_LTC_SymetricTree/main.go
@@ -6,17 +6,95 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+type Node struct {
+	data *TreeNode
+	next *Node
+}
+type Queue struct {
+	front *Node
+	last  *Node
+}
+
+// *
+//
+//	Recursive way
+//
+// /**
+// * Definition for a binary tree node.
+// * type TreeNode struct {
+// *     Val int
+// *     Left *TreeNode
+// *     Right *TreeNode
+// * }
+// */
+//
+//	func isSymmetric(root *TreeNode) bool {
+//	   if root == nil {
+//	       return true
+//	   }
+//	   return isMirror(root.Left, root.Right)
+//	}
+//
+//	func isMirror(a, b *TreeNode) bool {
+//	   // If both nodes are nil, they are mirrors of each other
+//	   if a == nil && b == nil {
+//	       return true
+//	   }
+//	   // If one of the nodes is nil, or their values differ, they aren't mirrors
+//	   if a == nil || b == nil || a.Val != b.Val {
+//	       return false
+//	   }
+//	   // Check if the left subtree of 'a' is a mirror of the right subtree of 'b' and vice versa
+//	   return isMirror(a.Left, b.Right) && isMirror(a.Right, b.Left)
+//	}
+//
+// */
+func (q *Queue) add(node *TreeNode) {
+	newNode := &Node{data: node}
+	if q.last == nil {
+		q.front = newNode
+		q.last = newNode
+	} else {
+		q.last.next = newNode
+		q.last = newNode
+	}
+}
+func (q *Queue) remove() *TreeNode {
+	if q.front == nil {
+		return nil
+	}
+	helper := q.front.data
+	q.front = q.front.next
+	if q.front == nil {
+		q.last = nil
+	}
+	return helper
+}
+func (q *Queue) isEmpty() bool {
+	return q.front == nil
+}
+
 func isSymmetric(root *TreeNode) bool {
-	leftRoot := root.Left
-	rightRoot := root.Right
-	for leftRoot != nil || rightRoot != nil {
-		if root.Left == nil || root.Right == nil {
+	if root == nil {
+		return true
+	}
+	queue := &Queue{}
+	queue.add(root.Left)
+	queue.add(root.Right)
+	for !queue.isEmpty() {
+		l := queue.remove()
+		r := queue.remove()
+		if l == nil && r == nil {
+			continue
+		}
+		if l == nil || r == nil || l.Val != r.Val {
 			return false
 		}
-		if leftRoot.Val != rightRoot.Val {
-			return false
-		}
-		leftRoot.
+
+		queue.add(l.Left)
+		queue.add(r.Right)
+		queue.add(l.Right)
+		queue.add(r.Left)
 	}
 	return true
 }
--- easy/101_LTC_SymetricTree/main_test.go
+++ easy/101_LTC_SymetricTree/main_test.go
@@ -3,8 +3,31 @@ package main
 import "testing"
 
 func TestIsSymetric(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-
+	var tree *TreeNode
+	var want bool
+	t.Run("Not Symmetric", func(t *testing.T) {
+		tree = &TreeNode{1, &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}, &TreeNode{Val: 2, Left: &TreeNode{Val: 3, Left: &TreeNode{Val: 5}}}}
+		want = false
+		got := isSymmetric(tree)
+		assert(t, got, want)
+	})
+	t.Run("Symmetric", func(t *testing.T) {
+		tree = &TreeNode{1, &TreeNode{2, &TreeNode{Val: 3}, &TreeNode{Val: 4}}, &TreeNode{2, &TreeNode{Val: 4}, &TreeNode{Val: 3}}}
+		want = true
+		got := isSymmetric(tree)
+		assert(t, got, want)
+	})
+	t.Run("Empty", func(t *testing.T) {
+		tree = &TreeNode{}
+		want = true
+		got := isSymmetric(tree)
+		assert(t, got, want)
+	})
+	t.Run("Not Center Symmetric", func(t *testing.T) {
+		tree = &TreeNode{1, &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}, &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}}
+		want = false
+		got := isSymmetric(tree)
+		assert(t, got, want)
 	})
 }
 

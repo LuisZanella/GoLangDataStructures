dda33746943d6e55f1895b5585759bdb4ae4da4f
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-01-31 11:21:28 -0600

    [ADD] Cracking the code excersice 1.2 comment

--- easy/CCI/1.2_CheckPermutation/main.go
+++ easy/CCI/1.2_CheckPermutation/main.go
@@ -4,6 +4,7 @@ func arePermutation(v []string) bool {
 	w1 := v[0]
 	w2 := v[1]
 	mapper := map[int32]int{}
+	// We can use a number array instead if we want
 	for _, c := range w1 {
 		mapper[c]++
 	}

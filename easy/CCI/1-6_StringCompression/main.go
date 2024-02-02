032d88a18d35a5f7f62e753017e7dfb02edd9f95
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-02-02 16:48:25 -0600

    [ADD] Compression algorithm

--- easy/CCI/1-6_StringCompression/main.go
+++ easy/CCI/1-6_StringCompression/main.go
@@ -9,14 +9,20 @@ func Commpression(s string) string {
 	var compression bytes.Buffer
 	var lastCharacter int32
 	var counter int
-	for _, v := range s {
-		counter++
-		if v != lastCharacter {
-			compression.WriteRune(v)
+	for i, v := range s {
+		if lastCharacter == 0 {
 			lastCharacter = v
+		}
+		if i == len(s)-1 {
+			counter++
+		}
+		if v != lastCharacter || i == len(s)-1 {
+			compression.WriteRune(lastCharacter)
 			compression.WriteString(strconv.Itoa(counter))
+			lastCharacter = v
 			counter = 0
 		}
+		counter++
 	}
 	return compression.String()
 }

93039b4d63412ac12fa7655bf474eee647103957
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-02-04 13:03:55 -0600

    [ADD] 2-1 Linked list clean code first execersice

--- easy/CCI/2-1_RemoveDups/main.go
+++ easy/CCI/2-1_RemoveDups/main.go
@@ -1,10 +1,28 @@
 package main
 
 type node struct {
-	value string
+	value int
 }
 
 type linkedList struct {
-	value node
-	next  *linkedList
+	node *node
+	next *linkedList
+}
+
+func removeDups(root *linkedList) *linkedList {
+	var m = map[int]bool{}
+	var p *linkedList
+	helper := root
+	for helper != nil && helper.node != nil {
+
+		if m[helper.node.value] {
+			p.next = p.next.next
+			helper = helper.next
+			continue
+		}
+		m[helper.node.value] = true
+		p = helper
+		helper = helper.next
+	}
+	return root
 }
--- easy/CCI/2-1_RemoveDups/main_test.go
+++ easy/CCI/2-1_RemoveDups/main_test.go
@@ -1,1 +1,86 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assert(t *testing.T, got, want *linkedList) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: %v Want: %v", got, want)
+		for got != nil && got.node != nil {
+			t.Errorf("Got: %d", got.node.value)
+			got = got.next
+		}
+		for want != nil && want.node != nil {
+			t.Errorf("Want: %d", want.node.value)
+			want = want.next
+		}
+	}
+}
+
+func TestRemoveDups(t *testing.T) {
+	cases := []struct {
+		name string
+		root *linkedList
+		want *linkedList
+	}{
+		{"empty", nil, nil},
+		{"nil list", &linkedList{nil, nil}, &linkedList{nil, nil}},
+		{"duplicates", &linkedList{&node{0},
+			&linkedList{&node{0},
+				&linkedList{&node{0},
+					&linkedList{&node{0},
+						&linkedList{&node{0},
+							&linkedList{&node{0},
+								nil}}},
+				}}}, &linkedList{&node{0}, nil}},
+		{"sorted duplicates", &linkedList{&node{1},
+			&linkedList{&node{1},
+				&linkedList{&node{2},
+					&linkedList{&node{10},
+						&linkedList{&node{10},
+							&linkedList{&node{15},
+								nil}}},
+				}}}, &linkedList{&node{1},
+			&linkedList{&node{2},
+				&linkedList{&node{10},
+					&linkedList{&node{15},
+						nil}}},
+		}},
+		{"unsorted", &linkedList{&node{15},
+			&linkedList{&node{3},
+				&linkedList{&node{3},
+					&linkedList{&node{15},
+						&linkedList{&node{5},
+							&linkedList{&node{15},
+								nil}}},
+				}}}, &linkedList{&node{15},
+			&linkedList{&node{3},
+				&linkedList{&node{5},
+					nil}}},
+		},
+		{"uniques", &linkedList{&node{1},
+			&linkedList{&node{2},
+				&linkedList{&node{3},
+					&linkedList{&node{10},
+						&linkedList{&node{5},
+							&linkedList{&node{15},
+								nil}}},
+				}}}, &linkedList{&node{1},
+			&linkedList{&node{2},
+				&linkedList{&node{3},
+					&linkedList{&node{10},
+						&linkedList{&node{5},
+							&linkedList{&node{15},
+								nil}}},
+				}}}},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := removeDups(c.root)
+			assert(t, got, c.want)
+		})
+	}
+
+}

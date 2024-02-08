0f10c6cbfb2f09aca213bd70339acb14f1426667
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-02-07 22:02:31 -0600

    [FIX] Removing infinite reference loop on parition

--- easy/CCI/2-4Partition/main.go
+++ easy/CCI/2-4Partition/main.go
@@ -5,20 +5,42 @@ type LinkedList struct {
 	next *LinkedList
 }
 
+//func partition(list *LinkedList, target int) *LinkedList {
+//	var head *LinkedList
+//	tail := list
+//	for list != nil {
+//		next := list.next
+//		if list.data < target {
+//			list.next = head
+//			head = list
+//		} else {
+//			tail.next = list
+//			tail = list
+//		}
+//		list = next
+//	}
+//	tail.next = nil
+//	return head
+//}
+
 func partition(list *LinkedList, target int) *LinkedList {
-	var head *LinkedList
-	tail := list
-	for list != nil {
-		next := list.next
-		if list.data < target {
-			list.next = head
-			head = list
-		} else {
-			tail.next = list
-			tail = list
-		}
-		list = next
+	if list == nil {
+		return list
 	}
-	tail.next = nil
-	return head
+	var head = &LinkedList{}
+	var tail = &LinkedList{}
+	htail := head
+	ttail := tail
+	for list != nil {
+		if list.data < target {
+			htail.next = list
+			htail = htail.next
+		} else {
+			ttail.next = list
+			ttail = ttail.next
+		}
+		list = list.next
+	}
+	htail.next = tail.next
+	return head.next
 }

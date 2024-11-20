92897ec840f2d4e955578fc5cf73371a8eb8fe75
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-11-20 17:11:15 -0600

    [ADD] Improve comments exercise 12

--- medium/12_LTC_IntegerToRoman/main.go
+++ medium/12_LTC_IntegerToRoman/main.go
@@ -4,7 +4,8 @@ package main
 //Medium
 //Topics
 //Companies
-//Seven different symbols represent Roman numerals with the following values:
+//Seven different symbols represent Roman numerals with the following
+//values:
 //
 //Symbol	Value
 //I	1
@@ -14,9 +15,23 @@ package main
 //C	100
 //D	500
 //M	1000
-//Roman numerals are formed by appending the conversions of decimal place values from highest to lowest. Converting a decimal place value into a Roman numeral has the following rules:
+//Roman numerals are formed by appending the conversions of decimal
+//place values from highest to lowest. Converting a decimal place value
+//into a Roman numeral has the following rules:
 //
-//If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
-//If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
-//Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+//If the value does not start with 4 or 9, select the symbol of the
+//maximal value that can be subtracted from the input, append that
+//symbol to the result, subtract its value, and convert the remainder
+//to a Roman numeral.
+//If the value starts with 4 or 9 use the subtractive form
+//representing one symbol subtracted from the following symbol,
+//for example, 4 is 1 (I) less than 5 (V): IV
+//and 9 is 1 (I) less than 10 (X): IX.
+//Only the following subtractive forms are
+//used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
+//Only powers of 10 (I, X, C, M) can be
+//appended consecutively at most 3 times to represent
+//multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D)
+//multiple times. If you need to append a symbol 4 times use the
+//subtractive form.
 //Given an integer, convert it to a Roman numeral.

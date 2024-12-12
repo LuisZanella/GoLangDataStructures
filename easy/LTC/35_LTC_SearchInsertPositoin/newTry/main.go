212e99fb7242c34f7cc7734c4a0b4a97f703786f
Luis Zanella <34582787+LuisZanella@users.noreply.github.com>
2024-12-11 21:12:24 -0600

    [ADD] New exercise to try vim and pogress

---
+++ easy/LTC/35_LTC_SearchInsertPositoin/newTry/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	return -1
+}
+
+func main() {
+	numbers := []int{-14, 32, 20, 19}
+
+	target := -15
+
+	fmt.Println(searchInsert(numbers, target))
+}
